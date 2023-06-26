522525c0d430079dfa1475d9c3612154f84ab253
DaviidSantos <89614359+DaviidSantos@users.noreply.github.com>
2023-06-26 17:12:20 -0300

    feat: created technologies component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-import { WhoAmI } from "@/components/whoami";
+import { Technologies } from '@/components/technologies';
+import { WhoAmI } from '@/components/whoami';
 
 export default function Home() {
-  return <main className="flex min-h-screen mt-10">
-    <WhoAmI />
-  </main>;
+  return (
+    <main className="min-h-screen mt-10">
+      <WhoAmI />
+      <div className="grid grid-cols-2">
+        <Technologies />
+        <div></div>
+      </div>
+    </main>
+  );
 }
---
+++ src/components/technologies.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const Technologies = () => {
+  return (
+    <section className="py-14">
+      <h4 className="bg-cyan-900 text-cyan-400 w-max tracking-wide">
+        Technologies
+      </h4>
+      <ul className="mt-5 flex flex-col gap-2">
+        <li className="text-zinc-300 text-sm tracking-wider">TypeScript</li>
+        <li className="text-zinc-300 text-sm tracking-wider">JavaScript</li>
+        <li className="text-zinc-300 text-sm tracking-wider">Next.js 13</li>
+        <li className="text-zinc-300 text-sm tracking-wider">React.js</li>
+        <li className="text-zinc-300 text-sm tracking-wider">TailwindCSS</li>
+        <li className="text-zinc-300 text-sm tracking-wider">Node.js</li>
+        <li className="text-zinc-300 text-sm tracking-wider">Express.js</li>
+      </ul>
+    </section>
+  );
+};
